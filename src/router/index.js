609e73770e192188e6e46708860f9250f872a1d6
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-07-27 12:14:26 +0530

    dsds

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import {createDrawerNavigator} from '@react-navigation/drawer'
 import HomeScreen from '../screen/HomeScreen'
+import Product from '../screen/Product'
 const drawer =createDrawerNavigator()
 export default function Router(){
     return(
         <NavigationContainer>
             <drawer.Navigator>
                 <drawer.Screen name="home" component={HomeScreen}/>
+                <drawer.Screen name="product" component={Product}/>
             </drawer.Navigator>
 
         </NavigationContainer>
---
+++ src/screen/Product.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Text} from 'react-native'
+export default function Product(){
+    return(
+        <>
+        <Text>Product</Text>
+        </>
+    )
+}
